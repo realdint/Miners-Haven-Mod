a04b8b5e8eb35392d94acf0d440b248a3839064f
realdint <141970776+realdint@users.noreply.github.com>
2023-08-12 14:54:30 -0400

    Update itemdatafetcher.js

--- itemdatafetcher.js
+++ itemdatafetcher.js
@@ -1,44 +1,48 @@
-fetch('https://realdint.github.io/Miners-Haven-Mod/items_data.json')
-.then(response => response.json())
-.then(data => {
-    console.log("Data fetched:", data);
-    const container = document.getElementById('items-container');
+fetch('https://raw.githubusercontent.com/realdint/Miners-Haven-Mod/main/items_data.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        const container = document.getElementById('items-container');
 
-    data.forEach(item => {
-        const itemDiv = document.createElement('div');
-        itemDiv.className = 'item';
+        data.forEach(item => {
+            const itemDiv = document.createElement('div');
+            itemDiv.className = 'item';
 
-        // Name
-        const itemName = document.createElement('h2');
-        itemName.textContent = item.ItemName;
-        itemDiv.appendChild(itemName);
+            // Name
+            const itemName = document.createElement('h2');
+            itemName.textContent = item.ItemName;
+            itemDiv.appendChild(itemName);
 
-        // Image
-        const itemImage = document.createElement('img');
-        itemImage.src = `https://www.roblox.com/asset-thumbnail/image?assetId=${item.ThumbnailId}&width=420&height=420&format=png`;
-        itemImage.alt = item.ItemName;
-        itemImage.className = 'thumbnail';
-        itemDiv.appendChild(itemImage);
+            // Image
+            const itemImage = document.createElement('img');
+            itemImage.src = `https://www.roblox.com/asset-thumbnail/image?assetId=${item.ThumbnailId}&width=420&height=420&format=png`;
+            itemImage.alt = item.ItemName;
+            itemImage.className = 'thumbnail';
+            itemDiv.appendChild(itemImage);
 
-        // Description
-        const itemDescription = document.createElement('p');
-        itemDescription.textContent = item.Description;
-        itemDiv.appendChild(itemDescription);
+            // Description
+            const itemDescription = document.createElement('p');
+            itemDescription.textContent = item.Description;
+            itemDiv.appendChild(itemDescription);
 
-        // Tier
-        const itemTier = document.createElement('p');
-        itemTier.textContent = `Tier: ${item.TierName}`;
-        itemDiv.appendChild(itemTier);
+            // Tier
+            const itemTier = document.createElement('p');
+            itemTier.textContent = `Tier: ${item.TierName}`;
+            itemDiv.appendChild(itemTier);
 
-        // Hitbox
-        const itemHitbox = document.createElement('p');
-        itemHitbox.textContent = `Hitbox - X: ${item.HitboxSize.X}, Y: ${item.HitboxSize.Y}, Z: ${item.HitboxSize.Z}`;
-        itemDiv.appendChild(itemHitbox);
+            // Hitbox
+            const itemHitbox = document.createElement('p');
+            itemHitbox.textContent = `Hitbox - X: ${item.HitboxSize.X}, Y: ${item.HitboxSize.Y}, Z: ${item.HitboxSize.Z}`;
+            itemDiv.appendChild(itemHitbox);
 
-        // Append to container
-        container.appendChild(itemDiv);
+            // Append to container
+            container.appendChild(itemDiv);
+        });
+    })
+    .catch(error => {
+        console.error('Error fetching the items:', error);
     });
-})
-.catch(error => {
-    console.error('Error fetching the items:', error);
-});
